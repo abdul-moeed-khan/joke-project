69a3f417de68d0938e5615a62104a7b69bcea27d
Huzaifa <huzaifanaeem909@gmail.com>
2024-07-26 14:50:12 +0500

    Add new.jsx

---
+++ src/New.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const New = () => {
+  return (
+    <div>
+      <h1>New File</h1>
+      <h3>This is a new file</h3>
+      <h2>This is also a new file</h2>
+    </div>
+  )
+}
+
+export default New
